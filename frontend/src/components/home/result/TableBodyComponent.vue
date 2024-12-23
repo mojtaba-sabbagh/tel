860234f345df37e099437b46bab120aae7dfdc61
Mahdi Heydari <mheydaridev@gmail.com>
2024-12-23 23:53:39 +0330

    front with tailwind

---
+++ frontend/src/components/home/result/TableBodyComponent.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  resultTableData: String,
+})
+</script>
+
+<template>
+  <tbody>
+    <tr class="border-b dark:border-gray-700">
+      <th scope="row" class="px-4 py-3">
+        <img
+          class="w-8 h-8 rounded-xl"
+          v-if="props.resultTableData.profile"
+          v-bind:src="props.resultTableData.profile"
+        />
+      </th>
+      <th scope="row" class="px-4 py-3 text-nowrap font-medium text-gray-900 dark:text-white">
+        {{ props.resultTableData.fullName }}
+      </th>
+      <td class="px-4 py-3 text-start text-nowrap">{{ props.resultTableData.extensionNumber }}</td>
+      <td class="px-4 py-3 text-start text-nowrap">{{ props.resultTableData.phoneNumber }}</td>
+      <td class="px-4 py-3 text-start text-nowrap">
+        {{ props.resultTableData.organizationalPosition }}
+      </td>
+      <td class="px-4 py-3 text-start text-nowrap">
+        {{ props.resultTableData.organizationalUnit }}
+      </td>
+      <td
+        class="px-4 py-3 text-start overflow-hidden text-ellipsis break-words whitespace-normal line-clamp-2"
+      >
+        {{ props.resultTableData.responsibility }}
+      </td>
+      <td class="px-4 py-3 text-start text-nowrap">{{ props.resultTableData.description }}</td>
+    </tr>
+  </tbody>
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import SearchComponent from '@/components/home/SearchComponent.vue'
+import ResultComponent from '@/components/home/ResultComponent.vue'
+</script>
+
+<template>
+  <div class="container mx-auto p-5">
+    <h2
+      class="text-gray-900 dark:text-white text-xl md:text-2xl font-bold my-4 md:my-10 border-s-8 border-s-gray-900 dark:border-s-white rounded-s-lg ps-2 leading-tight tracking-tight"
+    >
+      سامانه تلفن دانشگاه
+    </h2>
+
+    <SearchComponent></SearchComponent>
+    <ResultComponent></ResultComponent>
+  </div>
+</template>
