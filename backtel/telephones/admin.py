4c7f607c3e4c758f3bdff443743e70ef50ea8aaa
Mahdi Heydari <mheydaridev@gmail.com>
2024-12-27 19:19:27 +0330

    panel admin finished

--- backtel/telephones/admin.py
+++ backtel/telephones/admin.py
@@ -2,64 +2,90 @@ from django.contrib import admin
 from .models import UserProfile, Department, Telephone, PositionType, Position, Assign
 
 
-
 # Register your models here.
+class PositionInline(admin.TabularInline):
+    model = Position
+    extra = 1
+
+
+class AssignInline(admin.TabularInline):
+    model = Assign
+    extra = 1
+
+
 @admin.register(UserProfile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'first_name', 'last_name', 'gender', 'birthday', 'national_id', 'email', 'mobile']
-    list_filter = ['gender',]
+    readonly_fields = ['get_label_id']
+    list_display = ['get_label_id', 'user', 'first_name', 'last_name', 'gender', 'birthday', 'national_id', 'email', 'mobile']
+    list_filter = ['gender']
     search_fields = ['first_name', 'last_name', 'national_id', 'email', 'mobile']
-
-
+    inlines = [PositionInline]
+    
+    def get_label_id (self, obj):
+        return obj.id
+    get_label_id.short_description = 'آیدی'
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ['title', 'name', 'address', 'level', 'super_dep']
-    list_filter = ['title', 'level']
-    search_fields = ['name']
+    readonly_fields = ['get_label_id']
+    list_display = ['get_label_id', 'title', 'name', 'address', 'level', 'super_dep']
+    list_filter = ['title', 'level', 'super_dep']
+    search_fields = ['name', 'address']
+    inlines = [PositionInline]
 
-
+    def get_label_id (self, obj):
+        return obj.id
+    get_label_id.short_description = 'آیدی'
 
 
 @admin.register(Telephone)
 class TelephoneAdmin(admin.ModelAdmin):
-    # readonly_fields = ('id',)
-    # css = {
-    #          'all': ('templates/css/admin-extra.css ',)
-    #     }
-    search_fields = ['extension']
-
-
+    readonly_fields = ['get_label_id']
+    list_display = ['get_label_id', 'extension', 'complete_number', 'tel_address', 'description']
+    list_filter = [ 'tel_address']
+    search_fields = ['extension', 'complete_number', 'tel_address', 'description']
+    inlines = [AssignInline]
+    
+    def get_label_id (self, obj):
+        return obj.id
+    get_label_id.short_description = 'آیدی'
 
 
 @admin.register(PositionType)
 class PositionTypeAdmin(admin.ModelAdmin):
-    # readonly_fields = ('id',)
-    # css = {
-    #          'all': ('templates/css/admin-extra.css ',)
-    #     }
-    search_fields = ['title']
-
-
+    readonly_fields = ['get_label_id']
+    list_display = ['get_label_id', 'title', 'code', 'level']
+    list_filter = ['level']
+    search_fields = ['title', 'code']
+    inlines = [PositionInline]
+    
+    def get_label_id (self, obj):
+        return obj.id
+    get_label_id.short_description = 'آیدی'
 
 
 @admin.register(Position)
 class PositionAdmin(admin.ModelAdmin):
-    # readonly_fields = ('id',)
-    # css = {
-    #          'all': ('templates/css/admin-extra.css ',)
-    #     }
-    list_filter = ('dep',)
-    search_fields = ['dep__dep_name', 'owner__last_name', 'owner__first_name']
+    readonly_fields = ['get_label_id']
+    list_display = ['get_label_id', 'position_type', 'dep', 'owner', 'duties']
+    list_filter = ['dep__title', 'owner__gender']
+    search_fields = ['dep__name', 'position_type__title', 'owner__last_name', 'owner__first_name']
+    inlines = []
 
-
+    def get_label_id (self, obj):
+        return obj.id
+    get_label_id.short_description = 'آیدی'
 
 
 @admin.register(Assign)
 class AssignAdmin(admin.ModelAdmin):
-    # readonly_fields = ('id',)
-    # css = {
-    #          'all': ('templates/css/admin-extra.css ',)
-    #     }
-    search_fields = ['tel__extension', 'position__owner__last_name', 'position__dep__dep_name', 'position__owner__first_name']
+    readonly_fields = ('get_label_id',)
+    list_display = ['get_label_id', 'position', 'tel', 'date']
+    list_filter = ['position__dep__title', 'date']
+    search_fields = ['tel__extension', 'position__dep__name', 'position__position_type__title', 'position__owner__first_name', 'position__owner__last_name', 'position__owner__gender']
+
+    def get_label_id (self, obj):
+        return obj.id
+    get_label_id.short_description = 'آیدی'
+    
--- backtel/telephones/models.py
+++ backtel/telephones/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 # Create your models here.
 class UserProfile(models.Model):
     GENDER_CHOICES = (
@@ -19,17 +18,13 @@ class UserProfile(models.Model):
     email = models.CharField(max_length=255, blank=True, null=True, verbose_name='ایمیل')
     mobile = models.CharField(max_length=11, blank=True, null=True, verbose_name='تلفن همراه')
 
-
     class Meta:
         ordering = ['last_name', 'first_name']
         verbose_name = 'کاربر'
-        verbose_name_plural = 'مدیریت کاربران'
-        
+        verbose_name_plural = 'کاربران'
         
     def __str__(self):
-        return f'آیدی: {self.id}'
-
-
+        return f'{self.first_name} {self.last_name}'
 
 
 class Department(models.Model):
@@ -53,86 +48,69 @@ class Department(models.Model):
     level = models.IntegerField(default=0, verbose_name='سطح')
     super_dep = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, verbose_name='واحد های مرتبط')
     
-
     class Meta:
         ordering = ['title', 'name']
         verbose_name = 'واحد سازمانی'
-        verbose_name_plural = 'واحدهای سازمانی'
-        
+        verbose_name_plural = 'واحدهای سازمانی'    
     
     def __str__(self):
-        return f'آیدی: {self.id}'
-
-
+        return f"{self.title} - {self.name}"
 
 
 class Telephone(models.Model):
-    extension = models.CharField(max_length=255, unique=True)
-    complete_number = models.CharField(max_length=255, blank=True, null=True)
-    tel_address = models.CharField(max_length=255, blank=True, null=True)
-    description = models.CharField(max_length=255, blank=True, null=True)
-
+    extension = models.CharField(max_length=255, unique=True, verbose_name='افزونه')
+    complete_number = models.CharField(max_length=255, blank=True, null=True, verbose_name='تلفن کامل')
+    tel_address = models.CharField(max_length=255, blank=True, null=True, verbose_name='آدرس')
+    description = models.CharField(max_length=255, blank=True, null=True, verbose_name='توضیحات')
 
     class Meta:
-        ordering = ['extension']
+        ordering = ['extension', 'complete_number']
         verbose_name = "تلفن داخلی"
         verbose_name_plural = "تلفن‌های داخلی"
-        
-    
+            
     def __str__(self):
-        return f'آیدی: {self.id}'
-
-
+        return self.complete_number
 
 
 class PositionType(models.Model):
-    title = models.CharField(max_length=255)
-    code = models.CharField(max_length=255)
-    level = models.IntegerField(default=0)
-
+    title = models.CharField(max_length=255, verbose_name="عنوان")
+    code = models.CharField(max_length=255, verbose_name="کد")
+    level = models.IntegerField(default=0, verbose_name="سطح")
 
     class Meta:
         ordering = ['title']
         verbose_name = "نوع پست سازمانی"
         verbose_name_plural = "انواع پست های سازمانی"
 
-
     def __str__(self):
-        return f'آیدی: {self.id}'
-
-
+        return self.title
 
 
 class Position(models.Model):
-    position_type =  models.ForeignKey(PositionType, related_name='position_types', on_delete=models.CASCADE, verbose_name="the related position type")
-    dep = models.ForeignKey(Department, related_name='deps', on_delete=models.CASCADE, verbose_name="the related dep")
-    owner = models.ForeignKey(UserProfile, related_name='owners', on_delete=models.CASCADE, verbose_name="the related profile")
-    duties = models.CharField(max_length=255, blank=True, null=True, default='')
-
+    position_type =  models.ForeignKey(PositionType, related_name='position_types', on_delete=models.CASCADE, verbose_name="نوع پست سازمانی")
+    dep = models.ForeignKey(Department, related_name='deps', on_delete=models.CASCADE, verbose_name="واحد سازمانی")
+    owner = models.ForeignKey(UserProfile, related_name='owners', on_delete=models.CASCADE, verbose_name="کاربر مسئول")
+    duties = models.CharField(max_length=255, blank=True, null=True, default='', verbose_name='وظایف')
 
     class Meta:
         ordering = ['dep', 'position_type', 'owner']
         verbose_name = "پست سازمانی"
         verbose_name_plural = "پست‌های سازمانی"
 
-    
     def __str__(self):
-        return f'آیدی: {self.id}'
-
-
+        return f'{self.dep} - {self.position_type} - {self.owner}'
 
 
 class Assign(models.Model):
-    date = models.DateField(blank=True, null=True)
-    position = models.ForeignKey(Position, related_name='positions', on_delete=models.CASCADE, verbose_name="the related pos")
-    tel = models.ForeignKey(Telephone, related_name='tels', on_delete=models.CASCADE, verbose_name="the related tel")
-
+    date = models.DateField(blank=True, null=True, verbose_name="تاریخ تخصیص")
+    position = models.ForeignKey(Position, related_name='positions', on_delete=models.CASCADE, verbose_name="پست سازمانی")
+    tel = models.ForeignKey(Telephone, related_name='tels', on_delete=models.CASCADE, verbose_name="شماره تلفن")
 
     class Meta:
-        ordering = ['tel']
+        ordering = ['position', 'tel']
         verbose_name = "تخصیص تلفن"
         verbose_name_plural = "تخصیص تلفن‌ها"
 
-
     def __str__(self):
-        return f'آیدی: {self.id}'
+        return f'{self.position} - {self.tel}'
+    
